2ba860b64f2a260214edbaae225917ee44f6d334
unknown <mahalakshmianandan042gmail.com>
2025-09-25 12:41:08 +0530

    added

--- script.js
+++ script.js
@@ -37,3 +37,43 @@ function validatePassword(pass) {
 
     return true; // all rules passed
 }
+
+
+username.addEventListener('input', () => {
+    if (ValidateUserName(username.value)) {
+        username.classList.add('valid');
+        username.classList.remove('invalid');
+    } else {
+        username.classList.add('invalid');
+        username.classList.remove('valid');
+    }
+});
+
+// Real-time validation for Password
+password.addEventListener('input', () => {
+    if (validatePassword(password.value)) {
+        password.classList.add('valid');
+        password.classList.remove('invalid');
+    } else {
+        password.classList.add('invalid');
+        password.classList.remove('valid');
+    }
+});
+
+const redirectLink = "https://maha561.github.io/MSD-image_Gallery/";
+
+const form = document.querySelector('.fill');
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault(); // stop normal form submission
+
+    if (ValidateUserName(username.value) && validatePassword(password.value)) {
+        // redirect to new page if valid
+        window.location.href = redirectLink;
+    } else {
+        alert("Fix username or password first!");
+    }
+});
+
+
